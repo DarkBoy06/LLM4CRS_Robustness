5ef50c65eab3a5f43ac197ee3cf8f95d78eef5de
Anish Sawhney <76051087+DarkBoy06@users.noreply.github.com>
2026-02-09 15:23:48 +0530

    Implement chatcrs_pipeline for end-to-end processing

--- v0_rule_based/pipeline.py
+++ v0_rule_based/pipeline.py
@@ -1,1 +1,17 @@
+from .goal_planner import predict_goal
+from .knowledge_base import retrieve_knowledge
+from .response_generator import generate_response
 
+def chatcrs_pipeline(user_text):
+    """
+    End-to-end v0 ChatCRS pipeline
+    """
+    goal = predict_goal(user_text)
+    knowledge = retrieve_knowledge(goal, user_text)
+    response = generate_response(goal, knowledge)
+
+    return {
+        "goal": goal,
+        "knowledge": knowledge,
+        "response": response
+    }
